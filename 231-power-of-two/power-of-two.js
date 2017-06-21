d407899041fdb2b64cb7106d69f7a7e5cfc0b0e8
wj2061 <wj0x80d@gmail.com>
2017-06-21 05:04:24 -0400

    update at 2017-06-21

---
+++ 231-power-of-two/power-of-two.js
@@ -0,0 +1,23 @@
+// Given an integer, write a function to determine if it is a power of two.
+//
+//
+// Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    
+    while(n > 1){
+        var rest = n%2;
+        if(rest !== 0){
+            return false;
+        }
+        
+        n = Math.floor(n/2);
+    }
+    
+    return n === 1;
+};
